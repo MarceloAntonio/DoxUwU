17262004f0be81e8b6a38b12c50a267713f3d87d
MARCELO ANTONIO CABRAL MARTINS <marcelo_a_martins@estudante.sesisenai.org.br>
2025-07-02 20:56:39 -0300

    Foi criado arquivos para compactar arquivos principais

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,12 @@
-import { useNavigate } from "react-router-dom"
+import HomeMain from "./HomeMain"
 import Footer from "./Footer"
 
 function Home() {
-  const navigate = useNavigate()
-
-  const IrParaDoxing = () => {
-    navigate("/Doxing")
-  }
-
-  const IrParaSobre = () => {
-    navigate("/Sobre")
-  }
+  
 
   return (
     <div>
-      <main>
-        <div id="Home">
-          <img src="src\assets\FaceMuahaha.png" alt="" />
-          <h3> Clique e faça um DOXING em uma pessoa aleatória </h3>
-
-          <button onClick={IrParaDoxing}>Doxing</button>
-          <button onClick={IrParaSobre} id="SobreBtn">Sobre nós</button>
-        </div>
-      </main>
+      <HomeMain/>
       <Footer/>
     </div>
   )
---
+++ src/components/HomeMain.jsx
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom"
+function HomeMain(){
+
+const navigate = useNavigate()
+
+  const IrParaDoxing = () => {
+    navigate("/Doxing")
+  }
+
+  const IrParaSobre = () => {
+    navigate("/Sobre")
+  }
+
+return(
+<main>
+        <div id="Home">
+          <img src="src\assets\FaceMuahaha.png" alt="" />
+          <h3> Clique e faça um DOXING em uma pessoa aleatória </h3>
+
+          <button onClick={IrParaDoxing}>Doxing</button>
+          <button onClick={IrParaSobre} id="SobreBtn">Sobre nós</button>
+        </div>
+      </main>
+      )
+    
+}
+export default HomeMain
